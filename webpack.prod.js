adbc854f83d599891fe290adf6962f7788265d27
Jazbo Gross <jazbogross@gmail.com>
2024-08-20 20:46:38 +0100

    update

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,11 @@
-const { merge } = require("webpack-merge");
+const {merge} = require("webpack-merge");
 const path = require("path");
 
 const TerserPlugin = require("terser-webpack-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const common = require("./webpack.common.js");
 
-const PurgeCSSPlugin = require('@fullhuman/postcss-purgecss');
+const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   mode: "production",
@@ -15,37 +14,6 @@ module.exports = merge(common, {
     filename: "[name].[fullhash:5].js",
     chunkFilename: "[id].[fullhash:5].css",
     path: path.resolve(__dirname, "dist")
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          {
-            loader: "postcss-loader",
-            options: {
-              postcssOptions: {
-                plugins: [
-                  PurgeCSSPlugin({
-                    content: [
-                      path.join(__dirname, "src/**/*.html"),
-                      path.join(__dirname, "src/**/*.js"),
-                      path.join(__dirname, "src/**/*.jsx"),
-                      path.join(__dirname, "src/**/*.ts"),
-                      path.join(__dirname, "src/**/*.tsx"),
-                    ],
-                    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-                  }),
-                ],
-              },
-            },
-          },
-        ],
-      },
-    ],
   },
 
   optimization: {
@@ -57,14 +25,11 @@ module.exports = merge(common, {
         },
         exclude: /\/node_modules\//,
       }),
+      new MiniCssExtractPlugin({
+        filename: "[name].[fullhash:5].css",
+        chunkFilename: "[id].[fullhash:5].css"
+      }),
       new CssMinimizerPlugin(),
     ]
-  },
-
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "[name].[fullhash:5].css",
-      chunkFilename: "[id].[fullhash:5].css"
-    }),
-  ],
+  }
 });
